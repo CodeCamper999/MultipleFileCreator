fbce1015598b32307e8f154e7fe1cc57ea0bbf70
CodeCamper999 <randyhudsonjr13@gmail.com>
2025-01-09 17:47:28 -0600

    New Update
    The New Update program will also let choose a File name and add randoms letters at the end of that file so it can be automated

--- FileCreator.py
+++ FileCreator.py
@@ -1,19 +1,40 @@
+import random
 runTime = int(input("How many files would you like to create?: "))
 
+Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "I", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+CustomizableFile = input("Would you like to add your custom edits to the file as in directory, name, etc...[Y/N]: ")
 
-def CreateFiles(NumFiles):
-    NameFile = input("What is the name you want the file to have?: ")
-    WriteFile = input("What would you like to write in the file?: ")
     
-    with open(NameFile, "a") as Create:
-        Create.write(WriteFile)
-    for i in range(NumFiles):    
-        NameFile = input("Input Name of File again[E to exit]: ")
+def CreateFiles(NumFiles):
+
+    if CustomizableFile in ("y", "Y"):
+        NameFile = input("Where would you like the file to be please add name aswell: ")
+        WriteFile = input("What would you like to write in the file?: ")
+        
+        with open(NameFile, "a") as Create:
+            Create.write(WriteFile)
+            
+        for i in range(NumFiles):    
+            NameFile = input("Input Name of File again[E to exit]: ")
+            print(i)
+            
         if NameFile in ("e", "E"):
             print("Now Exiting...")
             exit()
-        with open(NameFile, "a") as NewCreate:
-            NewCreate.write(WriteFile)
+       
+    elif CustomizableFile in ("N", "n"):
+        Original = input("What is a file name(don't put .txt or list the directory)")
+        for i in range(runTime):
+            getLetter = random.choice(Alphabet[0:])
+            print(getLetter)
+            
+            with open(Original + getLetter + ".txt", "a") as Create:
+                Create.write("this is our new file")
+                
+            
+            
+            
+            
 
 CreateFiles(runTime)
