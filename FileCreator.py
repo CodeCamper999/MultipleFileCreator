ceda0013d4764d1500d3a00993ac3cefb0f84e62
CodeCamper999 <randyhudsonjr13@gmail.com>
2025-01-09 18:04:30 -0600

    Newest Update
    Users are about to customize the file they would like to create, but if they do not want to do that then they can go for the file generating method.

--- FileCreator.py
+++ FileCreator.py
@@ -1,21 +1,23 @@
-import random
+import random # importing the random module 
 runTime = int(input("How many files would you like to create?: "))
 
 Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "I", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+# Making a list of the Alphabet 
 CustomizableFile = input("Would you like to add your custom edits to the file as in directory, name, etc...[Y/N]: ")
-
+# Allowing user to pick if they was to make a custom file or let it automate 
     
 def CreateFiles(NumFiles):
 
-    if CustomizableFile in ("y", "Y"):
+    if CustomizableFile in ("y", "Y"): # If the user inputs y or Y then run this program
         NameFile = input("Where would you like the file to be please add name aswell: ")
         WriteFile = input("What would you like to write in the file?: ")
         
-        with open(NameFile, "a") as Create:
+        with open(NameFile, "a") as Create: # opening a new file and writing the name of the file the user wanted to create 
             Create.write(WriteFile)
+            # wrote what the user wanted me to write in that file 
             
         for i in range(NumFiles):    
+            # we will loop through this however many times the user would like 
             NameFile = input("Input Name of File again[E to exit]: ")
             print(i)
             
@@ -24,13 +26,24 @@ def CreateFiles(NumFiles):
             exit()
        
     elif CustomizableFile in ("N", "n"):
-        Original = input("What is a file name(don't put .txt or list the directory)")
+        Original = input("What is a file name(don't put .txt or list the directory): ")
+        directory = input("Custom Directory(\"C\")[C/D]? please press \"D\" to use default file save: ")
+        GetDirectory = input("put in directory(DO NOT use / at the end): ")
         for i in range(runTime):
             getLetter = random.choice(Alphabet[0:])
-            print(getLetter)
+            # we used random to choose a random letter to put at the end of the file
             
-            with open(Original + getLetter + ".txt", "a") as Create:
-                Create.write("this is our new file")
+            if directory in ("C", "c"):
+                
+                with open(GetDirectory + "/" + Original + getLetter + ".txt", "a") as originalfile:
+                    originalfile.write("New File in" + directory)
+            elif directory in ("D", "d"):
+                
+                with open(Original + getLetter + ".txt", "a") as Create:
+                    Create.write("this is our new file")
+            # we concat the file name the user choose, the random letter, and the .txt inorder to make it a text file 
+            
+            print(i)
                 
             
             
